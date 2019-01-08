bd7b763b60734befc6fc980189f2b23cf70376e5
Jon Grumer <jongrumer@gmail.com>
2019-01-08 15:52:15 +0100

    Extension of the libmod J fractional labels (1/2, 3/2, ...)
    From 32 to 40 entries, now up to J = 39/2

--- src/lib/libmod/jlabl_C.f90
+++ src/lib/libmod/jlabl_C.f90
@@ -1,8 +1,8 @@
-      MODULE jlabl_C 
+      MODULE jlabl_C_large
 !...Created by Pacific-Sierra Research 77to90  4.3E  06:16:25   2/14/04  
 !...Modified by Charlotte Froese Fischer 
 !                     Gediminas Gaigalas  10/05/17
-      CHARACTER(LEN=4), DIMENSION(32) :: JLBL, JLBR 
+      CHARACTER(LEN=4), DIMENSION(40) :: JLBL, JLBR 
       CHARACTER(LEN=4), DIMENSION(2) :: JLBP 
 !
 !   Left-justified strings
@@ -10,18 +10,20 @@
       DATA JLBL/'0   ', '1/2 ', '1   ', '3/2 ', '2   ', '5/2 ', '3   ', '7/2 ',&
                 '4   ', '9/2 ', '5   ', '11/2', '6   ', '13/2', '7   ', '15/2',&
                 '8   ', '17/2', '9   ', '19/2', '10  ', '21/2', '11  ', '23/2',&
-                '12  ', '25/2', '13  ', '27/2', '14  ', '29/2', '15  ', '31/2'/
+                '12  ', '25/2', '13  ', '27/2', '14  ', '29/2', '15  ', '31/2',&
+                '16  ', '33/2', '17  ', '35/2', '18  ', '37/2', '19  ', '39/2'/
 !
 !   Right-justified strings
 !
       DATA JLBR/'   0', ' 1/2', '   1', ' 3/2', '   2', ' 5/2', '   3', ' 7/2',&
                 '   4', ' 9/2', '   5', '11/2', '   6', '13/2', '   7', '15/2',&
                 '   8', '17/2', '   9', '19/2', '  10', '21/2', '  11', '23/2',&
-                '  12', '25/2', '  13', '27/2', '  14', '29/2', '  15', '31/2'/
+                '  12', '25/2', '  13', '27/2', '  14', '29/2', '  15', '31/2',&
+                '  16', '33/2', '  17', '35/2', '  18', '37/2', '  19', '39/2'/
 !
 !   Parity signs
 !
       DATA JLBP/ ' -  ', ' +  '/
 !
 
-      END MODULE jlabl_C 
+      END MODULE jlabl_C_large
